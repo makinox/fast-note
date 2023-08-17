63ad600541a3dcdd36e2de834c8ff2bf997bf1b1
Jesus Bossa <x@jesusbossa.dev>
2023-08-17 16:01:12 -0500

    feat: add cards

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,42 @@
+import { api } from '@/utils/api';
+
+type NoteCardProps = {
+  id: string;
+  title: string;
+  content: string;
+  completed: boolean;
+  onSuccess: () => void;
+};
+
+export const NoteCard = ({ title, content, id, completed, onSuccess }: NoteCardProps) => {
+  const completeMutation = api.notes.completeNote.useMutation({ onSuccess });
+  const unCompleteMutation = api.notes.unCompleteNote.useMutation({ onSuccess });
+  const deleteMutation = api.notes.deleteNote.useMutation({ onSuccess });
+
+  const handleComplete = () => completeMutation.mutate({ id });
+
+  const handleUnComplete = () => unCompleteMutation.mutate({ id });
+
+  const handleDelete = () => deleteMutation.mutate({ id });
+
+  return (
+    <article key={title} className="w-80 rounded-md bg-base-100 px-4 py-2 shadow-md">
+      <h2 className="mb-2 text-xl font-bold">{title}</h2>
+      <p>{content}</p>
+      <div className="mt-4 flex justify-between">
+        {completed ? (
+          <button className="btn btn-info btn-sm" onClick={handleUnComplete}>
+            {completeMutation.isLoading ? 'Descompletando...' : 'Completado'}
+          </button>
+        ) : (
+          <button className="btn btn-accent btn-sm" onClick={handleComplete}>
+            {unCompleteMutation.isLoading ? 'Completando...' : 'Completar'}
+          </button>
+        )}
+        <button className="btn btn-error btn-sm" onClick={handleDelete}>
+          {deleteMutation.isLoading ? 'Eliminando...' : 'Eliminar'}
+        </button>
+      </div>
+    </article>
+  );
+};
--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -1,7 +1,25 @@
+import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
 
 export const notesRouter = createTRPCRouter({
   getAllNotes: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.notes.findMany();
+  }),
+  completeNote: publicProcedure.input(z.object({ id: z.string() })).mutation(({ ctx, input }) => {
+    return ctx.prisma.notes.update({
+      where: { id: input.id },
+      data: { completed: true },
+    });
+  }),
+  unCompleteNote: publicProcedure.input(z.object({ id: z.string() })).mutation(({ ctx, input }) => {
+    return ctx.prisma.notes.update({
+      where: { id: input.id },
+      data: { completed: false },
+    });
+  }),
+  deleteNote: publicProcedure.input(z.object({ id: z.string() })).mutation(({ ctx, input }) => {
+    return ctx.prisma.notes.delete({
+      where: { id: input.id },
+    });
   }),
 });
